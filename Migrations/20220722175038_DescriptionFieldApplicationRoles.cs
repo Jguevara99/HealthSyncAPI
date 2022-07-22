02fa377100309bf358bf58091b51072a0b0364cb
DlopezS98 <01dlopezs98@gmail.com>
2022-07-22 16:43:34 -0600

    (migrations): application roles, description field

---
+++ Migrations/20220722175038_DescriptionFieldApplicationRoles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    public partial class DescriptionFieldApplicationRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "Security",
+                table: "Roles",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "Security",
+                table: "Roles");
+        }
+    }
+}
